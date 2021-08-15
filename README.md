fc6c84315ba94fe0b1fa55b12c06cb4d3c91730a
Dulitha Ranatunga <dulitharanatunga@gmail.com>
2021-08-15 18:11:13 +1000

    Initial commit - Add script for taking interest received in YouHodler and calculating the price in AUD

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# youhodlerTax
-Calculate income tax for youHodler deposits
+Calculates the interest from YouHodler converted to AUD at date received.
+
+Usage:
+
+0. npm install
+
+1. Prepare AUD/USD file:
+a. https://au.investing.com/currencies/usd-aud-historical-data 
+b. set date range for the table. Daily, Full Year
+c. copy table and save as `input/usd-aud.txt`
+
+2. Get YH transactions
+a. https://app.youhodler.com/history?limit=100&offset=0
+b. Adjust limit as neccessary
+c. Open dev tools and copy the json response for the page 
+d. Save as `input/YH.json` file. 
+
+3. Edit filenames in yhToAud.ts
+
+4. `npm start`
+5. Copy results to google sheets 
+
+
+Assumptions:
+- When I receive interest in YH, its considered as two different taxable events:
+
+1. "Income" - declared as the value of the coin in AUD (using market value). In this case, I'm assuming fair value for stable coins (USDC/USDT) to be 1:1 with the AUD-USD pair. I've put this under "Other Income" in my tax return. 
+2. Gain of a Asset (the coin) with a cost-base of this income price.
+
+When this asset is disposed (sold/traded), then we need to calculate the price (in AUD) after selling it, and pay CGT on that price - the cost base from (1).
+
+Based on the advice for staking rewards in 2021:
+https://community.ato.gov.au/t5/Cryptocurrency/Staking-rewards/td-p/37024
+
+
+
---
+++ yhToAud.ts
@@ -0,0 +1,111 @@
+var fs = require('fs');
+var dayjs = require('dayjs');
+var utc = require('dayjs/plugin/utc')
+var isBetween = require('dayjs/plugin/isBetween')
+var timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
+var customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+dayjs.extend(isBetween)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+function readFile(fn): string[] {
+    return fs.readFileSync(fn, 'utf8').split('\n').map(l => l.trim()).filter(l => !!l);
+}
+
+function parseUSDfile(usdToAudFile: string): Map<string, string> {
+    const r: Map<string, string> = new Map();
+    var lines: string[] = readFile(usdToAudFile);
+    lines.forEach(l => {
+        let [day, price] = l.split('\t').slice(0,2);
+        r.set(day.trim(), Number(price).toFixed(2));
+    });
+    return r;
+}
+
+
+// Legacy date file - From before I pulled from YH.
+function parseDate(badlyFormattedDate: string): string {
+    let [day,month,year]: any = badlyFormattedDate.split('/').slice(0,3);
+    if (year.length === 2) { 
+        year = '20'+year
+    };
+    if (day.length === 1) {
+        day = '0' + day;
+    }
+    if (month.length === 1) {
+        month = '0' + month;
+    }
+    // We manually do this in US dates because dayJs appears to be shit and i cbf fixing this.
+    const betterDate: string = `${month}-${day}-${year}`;
+    return dayjs(betterDate).format('MMM DD, YYYY');
+}
+
+function processDates() {
+    var dateFile = 'dates.txt';
+    const dates: string[] = readFile(dateFile);
+    const prices = parseUSDfile('usd-aud.txt');
+    const out: string[] = dates.map(l => {    
+        return l + "," + prices.get(parseDate(l));
+    });
+    writeOut('out.csv', out);
+}
+// processDates()
+
+interface YHJsonFormat {
+    id: string;
+    ticker: string;
+    version: string;
+    amount: string;
+    createdAt: string;
+    status: string;
+    fee: string;
+    type: string;
+    transactionId: string;
+    txHash: string;
+}
+
+function writeOut(path:string, lines: string[]) {    
+    fs.writeFileSync(path, lines.join('\n'));
+    console.log('Written to ' + path);
+}
+
+
+function yhJsonToSheetsCsv(audHistory:string, inYHFile: string, outYHFile: string) {
+    // In: YH.json, AUD on date file.
+    // Out: Interest, Date Acquired, Currency (e.g. USDC), Amount, FY, USD/AUD at Purchase, AUD (Cost Base for CGT), Sell Date, Sell Price, Brokerage (For Sale), USD/AUD at Sale, Brokerage (AUD), Discount Eligible, CGT
+
+    const prices = parseUSDfile('usd-aud.txt');
+    const rawData = fs.readFileSync(inYHFile);
+    const parsedYH: YHJsonFormat[] = JSON.parse(rawData).rows;
+    const headerRow: string = "Interest, Date Acquired, Currency (e.g. USDC), Amount, FY, USD/AUD at Purchase, AUD (Cost Base for CGT), Sell Date, Sell Price, Brokerage (For Sale), USD/AUD at Sale, Brokerage (AUD), Discount Eligible, CGT";
+    const out = parsedYH
+        .filter(x => x.type === 'SAVING_EARN')
+        .map(inRow => {
+            const outRow = [];
+            const date = dayjs(new Date(inRow.createdAt)).tz('Australia/Sydney');
+            const dateInAudFile = date.format('MMM DD, YYYY');
+            outRow.push('YouHodler');
+            outRow.push(date.format('DD/MM/YYYY')); 
+            outRow.push(inRow.ticker);
+            outRow.push(inRow.amount);
+            outRow.push(date.isBetween('2020-07-01','2021-07-01', null, '[)') ? '21':'22');
+            const price = prices.get(dateInAudFile);
+            if (!price) {
+                console.log('Skipped:' + dateInAudFile + ' Could Not Find AUD/USD Price');
+                return undefined;
+            } 
+            outRow.push(price);
+            outRow.push(Number(price) * Number(inRow.amount));
+            for (let i = 0; i < 7; i++)  {
+                outRow.push(""); // push empty rows
+            }
+            return outRow;
+        })
+        .filter(x => !!x)
+        .map(arr => arr.join(","))
+        .reverse();
+    writeOut(outYHFile, [headerRow, ...out]);
+}
+
+yhJsonToSheetsCsv('example/usd-aud.txt', 'example/exampleYH.json', 'output/exampleYH.out.csv')
